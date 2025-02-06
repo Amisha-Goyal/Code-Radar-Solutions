a8d1b8d21ad7488175ffa15d582d5c692408e3a8
Amisha Goyal <goyalamisha2006@gmail.com>
2025-02-06 11:28:19 +0530

    Solution to Find Day of the Week

---
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int day;
+    scanf("%d",&day);
+    if(n==0 ||n>7){
+        printf("Invalid");
+    }
+    switch(day){
+        case 1:
+        printf("Monday");
+        case 2:
+        printf("Tuesday");
+        case 3:
+        printf("Wednesday");
+        case 4:
+        printf("Thursday");
+        case 5:
+        printf("Friday");
+        case 6:
+        printf("Saturday");
+        case 7:
+        printf("Sunday");
+    }
+    
+}
