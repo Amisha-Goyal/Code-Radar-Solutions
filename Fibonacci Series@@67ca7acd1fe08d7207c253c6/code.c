4bfebcc12d11470fafda8cdc27bbdaea3c27aaa0
Amisha Goyal <goyalamisha2006@gmail.com>
2025-03-10 11:03:33 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -8,8 +8,8 @@ int fibonacciSeries(int n){
     else{
         return fibonacciSeries(n-1)+fibonacciSeries(n-2);
     }
-    for(int i=0;i<=n;i++){
-        printf("%d",fibonacciSeries(i));
-    }
+    // for(int i=0;i<=n;i++){
+    //     printf("%d",fibonacciSeries(i));
+    // }
     return 0;
 }
