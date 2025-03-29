6a0b5572c9e5a7d065d2775a0b98fd28e20e066f
Amisha Goyal <goyalamisha2006@gmail.com>
2025-03-29 22:49:16 +0530

    Solution to Print an Increasing Numeric Pyramid

--- Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
+++ Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
@@ -8,8 +8,9 @@ int main(){
             for(int k=1;k<=(2*i)-1;k++){
                 printf("%d",k);
             }
+            printf("\n");
         }
-        printf("\n");
+        
     
     return 0;
 }
