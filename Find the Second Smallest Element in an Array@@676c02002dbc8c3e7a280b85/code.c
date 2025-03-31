d792927106132d09bda9968928bafcf196c38e18
Amisha Goyal <goyalamisha2006@gmail.com>
2025-03-31 23:46:52 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -1,24 +1,48 @@
-#include<stdio.h>
-#include<limits.h>
-int main(){
-    int n,small=INT_MAX,second=INT_MAX,arr[20];
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+#include <stdio.h>
+#include <limits.h>  // For INT_MAX
+
+int main() {
+    int n, i;
+    
+    // Input the number of elements in the array
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    // If there are fewer than 2 elements, there's no second smallest number
+    if (n < 2) {
+        printf("Array must have at least two elements.\n");
+        return 0;
     }
-    for(int i=0;i<n;i++){
-        if(arr[i]<small){
-            second=small;
-            small=arr[i];
-        }
-        else if(arr[i]<second && arr[i]!=small){
-            if(second==INT_MAX){
-                printf("-1");
-            }
-            else{
-                printf("%d",second);
-            }
-            
+
+    int arr[n];
+    
+    // Input the array elements
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Initialize smallest and second smallest as INT_MAX
+    int smallest = INT_MAX;
+    int second_smallest = INT_MAX;
+
+    // Traverse the array to find the smallest and second smallest
+    for (i = 0; i < n; i++) {
+        // Update the smallest and second smallest
+        if (arr[i] < smallest) {
+            second_smallest = smallest; // Update second smallest
+            smallest = arr[i];          // Update smallest
+        } else if (arr[i] < second_smallest && arr[i] != smallest) {
+            second_smallest = arr[i];   // Update second smallest
         }
     }
+
+    // If there is no second smallest (all elements are the same)
+    if (second_smallest == INT_MAX) {
+        printf("-1");
+    } else {
+        printf("%d", second_smallest);
+    }
+
+    return 0;
 }
